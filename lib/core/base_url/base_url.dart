bab5366f57e1af6bd4d3802502751213a5a349ef
Sabbir <sabbirsamolla51@gmail.com>
2026-03-30 00:50:54 +0600

    base url change

--- lib/core/base_url/base_url.dart
+++ lib/core/base_url/base_url.dart
@@ -1,4 +1,5 @@
-// final String baseUrl = 'https://dpi.spondater.net';
-// final String wsBaseUrl = 'wss://dpi.spondater.net';
-final String baseUrl = 'https://clubby-andy-irksomely.ngrok-free.dev';
-final String wsBaseUrl = 'wss://clubby-andy-irksomely.ngrok-free.dev';
+// final String baseUrl = 'https://clubby-andy-irksomely.ngrok-free.dev';
+// final String wsBaseUrl = 'wss://clubby-andy-irksomely.ngrok-free.dev';
+
+final String baseUrl = 'https://coppnet.org';
+final String wsBaseUrl = 'wss://coppnet.org';
